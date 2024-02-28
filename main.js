6152489ba02450fa2e3480599e3187aa36cb9c42
Ferrari050 <jvm.ferrari@hotmail.com>
2024-02-28 11:41:25 -0300

    testando

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const cells = document.querySelectorAll("[cell]");
 const statusDisplay = document.getElementById("status");
 let currentPlayer = "X";
-let isGameRunning = 1;
 
 let gameState = ["", "", "", "", "", "", "", "", ""];
 
@@ -20,6 +19,36 @@ let contador = 0;
 
 const buttons = ["11", "12", "13", "21", "22", "23", "31", "32", "33"];
 
+const botao11 = document.getElementById(buttons[0]);
+const botao12 = document.getElementById(buttons[1]);
+const botao13 = document.getElementById(buttons[2]);
+const botao21 = document.getElementById(buttons[3]);
+const botao22 = document.getElementById(buttons[4]);
+const botao23 = document.getElementById(buttons[5]);
+const botao31 = document.getElementById(buttons[6]);
+const botao32 = document.getElementById(buttons[7]);
+const botao33 = document.getElementById(buttons[8]);
+
+function checkX() {
+  if (
+    botao11.textContent.includes("X") &&
+    botao12.textContent.includes("X") &&
+    botao13.textContent.includes("X")
+  ) {
+    alert("o jogo acabou.");
+  }
+}
+
+function checkO() {
+  if (
+    botao11.textContent.includes("O") &&
+    botao12.textContent.includes("O") &&
+    botao13.textContent.includes("O")
+  ) {
+    alert("o jogo acabou. O é o vencedor");
+  }
+}
+
 while (contador < buttons.length) {
   const botao = document.getElementById(buttons[contador]);
   botao.addEventListener("click", function () {
@@ -34,23 +63,5 @@ while (contador < buttons.length) {
     statusDisplay.innerHTML = `É a vez do jogador : ${currentPlayer}`;
   });
   contador = contador + 1;
-}
-
-const botao11 = document.getElementById(buttons[0]);
-const botao12 = document.getElementById(buttons[1]);
-const botao13 = document.getElementById(buttons[2]);
-const botao21 = document.getElementById(buttons[3]);
-const botao22 = document.getElementById(buttons[4]);
-const botao23 = document.getElementById(buttons[5]);
-const botao31 = document.getElementById(buttons[6]);
-const botao32 = document.getElementById(buttons[7]);
-const botao33 = document.getElementById(buttons[8]);
-
-if (
-  botao11.textContent.includes("X") &&
-  botao12.textContent.includes("X") &&
-  botao13.textContent.includes("X")
-) {
-  console.log("o jogo acabou");
-  isGameRunning = 0;
+  
 }
